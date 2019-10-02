9164beb1d8758bd57ce3af33f941abcbc2ae7a3a
Ruslan <rus.efremoov@gmail.com>
2019-10-02 16:14:40 +0300

    lb1

---
+++ lb 1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lb_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, result;
+            char oper;
+
+            Console.WriteLine("Введите первое число");
+            a = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите оператор");
+            oper = Convert.ToChar(Console.ReadLine());
+
+            Console.WriteLine("Введите второе число");
+            b = Convert.ToDouble(Console.ReadLine());
+
+            if (oper == '+')
+            {
+                result = a + b;
+                Console.WriteLine(a + "+" + b + "=" + result);
+            }
+            else if (oper == '-')
+            {
+                result = a - b;
+                Console.WriteLine(a + "-" + b + "=" + result);
+            }
+            else if (oper == '*' )
+            {
+                result = a * b;
+                Console.WriteLine(a + "*" + b + "=" + result);
+            }
+            else if (oper == '/')
+            {
+                result = a / b;
+                Console.WriteLine(a + "/" + b + "=" + result);
+            }
+            else if (oper == '=')
+            {
+                result = a;
+                Console.WriteLine(result);
+            }
+            else
+            {
+                Console.WriteLine("Неизвестный оператор");
+            }
+
+        }
+    }
+}
